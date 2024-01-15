2a158b724d7dabd157c1844b7db0e79ce4e735d3
Darkhackit <earthur510@gmail.com>
2024-01-15 09:36:24 +0000

    vendors update

--- resources/ts/router/index.ts
+++ resources/ts/router/index.ts
@@ -5,7 +5,7 @@ import routes from '~pages'
 import { canNavigate } from '@layouts/plugins/casl'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("/"),
   routes: [
     {
       path: '/',
